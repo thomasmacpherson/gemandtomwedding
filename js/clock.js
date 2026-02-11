db9384c683df31a4d30a171b994d746165a89d91
Thomas Macpherson-Pope <thomasmacpherson@users.noreply.github.com>
2026-02-11 21:05:07 +0000

    Form test

--- js/clock.js
+++ js/clock.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
   let currentDate = new Date();
 
   // Target future date/24 hour time/Timezone
-  let targetDate = moment.tz("2026-05-01 14:00", "London/England");
+  let targetDate = moment.tz("2026-05-01 14:00", "Europe/London");
 
   // Calculate the difference in seconds between the future and current date
   let diff = targetDate / 1000 - currentDate.getTime() / 1000;
